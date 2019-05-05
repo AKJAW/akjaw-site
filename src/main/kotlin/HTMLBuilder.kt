5eab57e6ae227efeb5ed12f56c6b20def5c4b392
AKJAW <akjaworski1@gmail.com>
2019-05-05 22:14:27 +0200

    created dsl tag builder using lambdas with reciever

--- src/main/kotlin/HTMLBuilder.kt
+++ src/main/kotlin/HTMLBuilder.kt
@@ -1,3 +1,66 @@
 package akjaw
 
-//class HTMLBuilder(val projectList: List)
+import akjaw.Model.Translation
+
+class HTMLBuilder(val projectList: List<List<Translation>>){
+//    val html: Tag
+
+    init{
+//        html = Tag("html").apply{
+//            add(createHead())
+//            add(createBody())
+//        }
+//        val head = Tag("head")
+//        html.add(createBody())
+
+//        val htmlTree = html {
+//            body {
+//                + "Ale bomba"
+//            }
+//        }
+
+        val htmlTree = tag("html") {
+            tag("body") {
+                + "Ale bomba"
+            }
+        }
+
+        println(htmlTree)
+    }
+
+    private fun tag(name: String, init: Tag.() -> Tag?): Tag{
+        return Tag(name).apply {
+            add(init())
+        }
+    }
+
+    class Tag(private val name: String){
+        private val tagList = mutableListOf<Tag>()
+        private var textContent: String = ""
+
+        fun add(tag: Tag?){
+            tag?.let {
+                tagList.add(it)
+            }
+        }
+
+        operator fun String.unaryPlus(): Tag?{
+            textContent += this
+            return null
+        }
+
+        override fun toString(): String {
+            return """<$name>${tagList.fold("") {acc, tag -> "$acc$tag"}}$textContent</$name>"""
+        }
+    }
+
+
+}
+
+fun main(){
+    val mutableList = mutableListOf<List<Translation>>()
+    mutableList.add(listOf(Translation(language="pl", name="Logo-quiz web", description="no fajna apka"), Translation(language="en", name="Logo-quiz web", description="cool app")))
+    mutableList.add(listOf(Translation(language="pl", name="akjTimer", description="ayy"), Translation(language="en", name="akjTimer", description="lmaro"), Translation(language="en", name="Logo-quiz web", description="cool app")))
+
+    HTMLBuilder(mutableList)
+}
