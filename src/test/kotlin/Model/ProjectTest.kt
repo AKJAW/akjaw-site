94e21bf51ecd7ae3bf329aed8b99b01ba0b544fd
AKJAW <akjaworski1@gmail.com>
2019-06-30 11:01:56 +0200

    added Project tests / deleted Railway package

---
+++ src/test/kotlin/Model/ProjectTest.kt
@@ -0,0 +1,52 @@
+package Model
+
+import akjaw.JsonParser
+import akjaw.Model.Project
+import com.beust.klaxon.JsonObject
+import com.beust.klaxon.Parser
+import com.google.common.truth.Truth
+import org.junit.Test
+import java.lang.ClassCastException
+import java.lang.NullPointerException
+
+class ProjectTest{
+
+    @Test
+    fun `it sets the root key as the project name`(){
+        val root = JsonObject(mapOf("name" to JsonObject()))
+
+        val project = Project(root)
+
+        Truth.assertThat(project.name).isEqualTo("name")
+    }
+
+    @Test
+    fun `projectData contains all nodes inside root value`(){
+        val div = JsonObject(mapOf("h1" to "header", "span" to "span"))
+        val root = JsonObject(mapOf("name" to div))
+
+        val project = Project(root)
+
+        Truth.assertThat(project.projectData).hasSize(2)
+        Truth.assertThat(project.projectData["h1"]).isEqualTo("header")
+        Truth.assertThat(project.projectData["span"]).isEqualTo("span")
+    }
+
+    @Test(expected = ClassCastException::class)
+    fun `if root value is not a nested object then throw an error`(){
+        val root = JsonObject(mapOf("name" to 1))
+        val project = Project(root)
+    }
+
+    @Test(expected = NoSuchElementException::class)
+    fun `if an empty object is passed throw an error`(){
+        val root = JsonObject()
+        val project = Project(root)
+    }
+
+    @Test(expected = NullPointerException::class)
+    fun `if root has a null value throw an error`(){
+        val root = JsonObject(mapOf("name" to null))
+        val project = Project(root)
+    }
+}
