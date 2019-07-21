e4adb95fe6b74363906d2af60f47ce21fb4f3609
AKJAW <akjaworski1@gmail.com>
2019-07-21 20:25:27 +0200

    added language toggle logic

--- build/main.js
+++ build/main.js
@@ -1,3 +1,8 @@
+LANGUAGE = {
+    "pl-button": "pl",
+    "en-button": "en"
+}
+
 document.addEventListener("DOMContentLoaded", function(event) {
     setLaungageButtonsListener()
     console.log("asd")
@@ -10,12 +15,13 @@ function setLaungageButtonsListener(){
 function addEventListenerQuery(query, event, fn) {
     var list = document.querySelectorAll(query);
     for (var i = 0, len = list.length; i < len; i++) {
-        list[i].addEventListener(event, fn, false);//wrapper na fn, i twórz własny object Node do którego dołącz remove add class
+        list[i].addEventListener(event, fn, false);
     }
 }
 
 function onLanguageButtonClick(event){
-    toggleButtonActivation(event.target)
+    const activatedLanguage = toggleButtonActivation(event.target)
+    toggleLanguageText(activatedLanguage)
     //TODO pokaz i ukryj tekst tak jak toggleButtonActivation
 }
 
@@ -23,25 +29,36 @@ function onLanguageButtonClick(event){
 function toggleButtonActivation(button) {
     button.addClass("active")
 
-    if (button.previousSibling) {
-        button.previousSibling.removeClass("active")
-    } else if (button.nextSibling) {
-        button.nextSibling.removeClass("active")
+    const sibling = button.getExistingSibling()
+    sibling.removeClass("active")
+
+    return LANGUAGE[button.id]
+}
+
+function toggleLanguageText(activatedLanguage){
+    textNodes = document.querySelectorAll("." + activatedLanguage)
+
+    for (var i = 0, len = textNodes.length; i < len; i++) {
+        console.log(textNodes[i])
+        textNodes[i].toggleLanguage()
     }
 }
 
-Object.prototype.removeClass = function(className){
-    if(!this.classList){
-        throw new Error("incorrect object")
-    }
+HTMLElement.prototype.toggleLanguage = function(){
+    this.removeClass("none")
 
+    const sibling = this.getExistingSibling()
+    sibling.addClass("none")
+}
+
+HTMLElement.prototype.removeClass = function(className){
     this.classList.remove(className)
 }
 
-Object.prototype.addClass = function(className){
-    if(!this.classList){
-        throw new Error("incorrect object")
-    }
-
+HTMLElement.prototype.addClass = function(className){
     this.classList.add(className)
+}
+
+HTMLElement.prototype.getExistingSibling = function(){
+    return this.previousSibling || this.nextSibling
 }
